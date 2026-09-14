d32c0d04bf48cb2024030b80760a7b392474510a
nikhatparveen1 <nikhatparveen2k1@gmail.com>
2026-09-14 23:05:13 +0530

    Day 22: add route table and subnet association to main.tf

--- day-19/terraform-subnets/main.tf
+++ day-19/terraform-subnets/main.tf
@@ -45,3 +45,20 @@ resource "aws_internet_gateway" "igw" {
     Name = "day-20-igw"
   }
 }
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "day-22-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_sub.id
+  route_table_id = aws_route_table.public_rt.id
+}
